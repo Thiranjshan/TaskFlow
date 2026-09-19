5481e4753f60fbcefcc32526ba61d8cf3b2c93f9
Thiranjshan <thiranjayaishan@gmail.com>
2026-09-19 15:21:18 +0530

    test for ci cd

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -162,7 +162,7 @@ export default function Dashboard() {
             <div className="brand-icon-box">
               <IconListTodo size={22} />
             </div>
-            <span className="brand-title">TaskFlows</span>
+            <span className="brand-title">TaskFlow</span>
           </div>
 
           <div className="navbar-actions">
